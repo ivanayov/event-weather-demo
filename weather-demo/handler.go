c5f549ce339bcf0408a33f043d5589d3b120e7f7
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-11-14 10:09:34 +0000

    Rename template->html
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- weather-demo/handler.go
+++ weather-demo/handler.go
@@ -1,3 +1,6 @@
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package function
 
 import (
@@ -6,7 +9,6 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
-	"os"
 )
 
 type CalendarEvent struct {
@@ -29,20 +31,13 @@ type Event struct {
 func Handle(req []byte) string {
 
 	var err error
-	tmpl, err := template.ParseFiles("./template/index.html")
+	tmpl, err := template.ParseFiles("./html/index.html")
 	if err != nil {
 		return fmt.Sprintf("Internal server error with homepage: %s", err.Error())
 	}
 	var tpl bytes.Buffer
 
-	events := []Event{
-		{
-			City: "Sofia",
-			Name: "OpenFest",
-		},
-	}
-
-	realEventBytes, readFileErr := ioutil.ReadFile("./template/events.json")
+	realEventBytes, readFileErr := ioutil.ReadFile("./html/events.json")
 	if readFileErr != nil {
 		return readFileErr.Error()
 	}
@@ -52,8 +47,11 @@ func Handle(req []byte) string {
 	if unmarshalErr != nil {
 		return unmarshalErr.Error()
 	}
-
-	os.Stderr.Write(realEventBytes)
+	// os.Stderr.Write(realEventBytes)
+	events := []Event{}
+	for _, realEvent := range realEvents {
+		events = append(events, Event{City: realEvent.Location, Name: realEvent.Content})
+	}
 
 	err = tmpl.Execute(&tpl, HomepageTokens{
 		Events: events,
